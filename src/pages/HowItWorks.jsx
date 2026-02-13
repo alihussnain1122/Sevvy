55fbf3262aa8f3c03ee0ffba89c20b59616db66a
Ali <alihussnain.contact@gmail.com>
2026-02-14 03:02:27 +0500

    working howitsworks centeralization

--- src/pages/HowItWorks.jsx
+++ src/pages/HowItWorks.jsx
@@ -1,9 +1,11 @@
+import Centralization from '../components/Works/Centeralize';
 import Hero from '../components/Works/Hero';
 
 export default function HowItWorks() {
   return (
     <div>
       <Hero />
+      <Centralization />
     </div>
   );
 }
