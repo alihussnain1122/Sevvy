d7c32da77eb31e1fea8e557673d8a90055350b28
Ali <alihussnain.contact@gmail.com>
2026-02-15 21:14:44 +0500

    updates routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Blog from './pages/Blog';
-import Pricing from './pages/Pricing';
-import HowItWorks from './pages/HowItWorks';
-
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/how-it-works" element={<HowItWorks />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/blogs" element={<Blog />} />
-     
-      </Routes>
+      <AppRoutes />
     </>
   );
 }
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Blog from '../pages/Blog';
+import Pricing from '../pages/Pricing';
+import HowItWorks from '../pages/HowItWorks';
+
+const AppRoutes = () => (
+	<Routes>
+		<Route path="/" element={<Home />} />
+		<Route path="/how-it-works" element={<HowItWorks />} />
+		<Route path="/pricing" element={<Pricing />} />
+		<Route path="/blogs" element={<Blog />} />
+	</Routes>
+);
+
+export default AppRoutes;
