ac6194653986c65e5ee7b2afd5480c7bb8759a22
Ali <alihussnain.contact@gmail.com>
2026-02-27 21:50:13 +0500

    update navbar issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <div className="pt-[118px]">
         <AppRoutes />
